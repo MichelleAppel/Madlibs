5dbf7797551ce7addc2d76dc053aa15325d2cadb
Michelle <appel.michelle@gmail.com>
2016-11-08 21:46:25 +0100

    Passes on story to second activity

--- app/src/main/java/com/example/michelle/madlib/WelcomeScreen.java
+++ app/src/main/java/com/example/michelle/madlib/WelcomeScreen.java
@@ -3,10 +3,16 @@ package com.example.michelle.madlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class WelcomeScreen extends AppCompatActivity {
+
+    static final String STORY = "story";
+    protected Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +22,33 @@ public class WelcomeScreen extends AppCompatActivity {
 
     // Called when the user presses the "Get started" button
     public void start(View view) {
-        Intent intent = new Intent(WelcomeScreen.this, WordActivity.class);
+        // Make a story object
+        story = read_story("madlib0_simple");
+
+        // Go to next activity
+        Intent intent = new Intent(this, WordActivity.class);
+        intent.putExtra(STORY, story);
         startActivity(intent);
     }
 
+    // gets input stream from story file
+    public Story read_story(String filename) {
+        Story story = null;
+        InputStream inputStream = null;
+        try {
+            inputStream = getAssets().open(filename + ".txt");
+            story = new Story(inputStream);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        return story;
+    }
 }
--- app/src/main/java/com/example/michelle/madlib/WordActivity.java
+++ app/src/main/java/com/example/michelle/madlib/WordActivity.java
@@ -4,13 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class WordActivity extends AppCompatActivity {
+
+    static final String STORY = "story";
+    protected Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
+
+        Bundle extras = getIntent().getExtras();
+        story = (Story) extras.getSerializable(STORY);
+
+        if (story != null) {
+            // Restore value of members from saved state
+            TextView wordCounter = (TextView)findViewById(R.id.TextViewWordCounter);
+            wordCounter.setText("Nog 5");
+        } else {
+            Intent intent = new Intent(this, WelcomeScreen.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void ok(View view) {
